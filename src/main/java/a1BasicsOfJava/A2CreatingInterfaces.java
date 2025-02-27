c21281e3cfba31acc6553cb91e4299c2f515845b
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2025-02-27 18:45:52 +0530

    basics

---
+++ src/main/java/a1BasicsOfJava/A2CreatingInterfaces.java
@@ -0,0 +1,33 @@
+package a1BasicsOfJava;
+
+public interface A2CreatingInterfaces {
+
+	public interface Interface1 {
+
+	}
+	
+//	private interface Interface2 {
+//
+//	}
+	
+//	protected interface Interface3 {
+//
+//	}
+	
+	interface Interface4 {
+
+	}
+	
+}
+
+//private interface A2CreatingInterfaces {
+//
+//}
+
+//protected interface A2CreatingInterfaces {
+//
+//}
+
+interface A2CreatingInterfaces1 {
+
+}
