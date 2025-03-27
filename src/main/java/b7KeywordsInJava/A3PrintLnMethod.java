5194ac576de2840eb9b95809eb93e1f32462d8de
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2025-03-27 22:48:27 +0530

    final and this

---
+++ src/main/java/b7KeywordsInJava/A3PrintLnMethod.java
@@ -0,0 +1,21 @@
+package b7KeywordsInJava;
+
+public class A3PrintLnMethod {
+
+	public static void main(String[] args) {
+
+//		use of println method
+		System.out.println("abc");
+		System.out.println();
+		System.out.println("xyz");
+		
+//		PrintStream print = new PrintStream();
+//		There are two changes because of which we could not create object for the class
+//		1. The default constructor PrintStream() could be private
+//		2. PrintStream class may have only parameterized constructors. 
+		
+		System.out.println();
+		
+	}
+
+}
---
+++ src/main/java/b7KeywordsInJava/A4MainMethod.java
@@ -0,0 +1,20 @@
+package b7KeywordsInJava;
+
+public class A4MainMethod {
+	
+//	JVM needs main method in exact format 
+//	JVM executes each line from main method through main thread.
+//	We can overload main method
+//	We can not override main method because its static
+//	The reason for main method to be static is, to be called by JVM without creating object for the class.
+
+//	public static void main(String[] args) {
+	static public void main(String[] args) {
+		
+	}
+	
+	public void main() {
+		
+	}
+
+}
